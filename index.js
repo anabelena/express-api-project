ee2e2a9166ba541f32c7bd98490d42a77a215c1f
anabelena <anabelen.aristah@gmail.com>
2026-02-15 22:33:33 -0800

    add readme

--- index.js
+++ index.js
@@ -1,5 +1,29 @@
 const express = require("express");
+
 const app = express(); //create server
+
+// GET -> Cliente espera datos
+app.get("/", (req, res) => {
+  //   // sendfile(ruta archivo,objeto de opciones)
+  //   res.sendFile("./static/index.html", {
+  //     root: __dirname,
+  //   });
+
+  // res.end(""Hello world!");
+  res.json({
+    name: "belen",
+    password: "hola",
+    age: 40,
+    points: [2, 8, 9],
+  });
+});
+
+app.get("/isalive", (req, res) => {
+  res.sendStatus(204);
+});
+
+//Cliente envia datos al Server
+app.post();
 
 app.listen(3000, () => {
   console.log("Server running on http://localhost:3000");
