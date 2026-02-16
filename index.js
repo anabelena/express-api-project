9ada50a33152281a0d4a6b4df4570de722ba334d
anabelena <anabelen.aristah@gmail.com>
2026-02-16 15:03:51 -0800

    feat(middleware): add logger and isAuthenticated middleware.

--- index.js
+++ index.js
@@ -2,28 +2,34 @@ const express = require("express");
 
 const app = express(); //create server
 
-// GET -> Cliente espera datos
-app.get("/", (req, res) => {
-  //   // sendfile(ruta archivo,objeto de opciones)
-  //   res.sendFile("./static/index.html", {
-  //     root: __dirname,
-  //   });
+// Middleware => Functions que se ejecutan previas a la respuesta del servidor
 
-  // res.end(""Hello world!");
-  res.json({
-    name: "belen",
-    password: "hola",
-    age: 40,
-    points: [2, 8, 9],
-  });
+// middleware: logger
+app.use((req, res, next) => {
+  console.log(`Logger => Route:${req.url}, Method:${req.method}`);
+  next();
 });
 
-app.get("/isalive", (req, res) => {
-  res.sendStatus(204);
+app.get("/profile", (req, res) => {
+  res.send("Profile page");
 });
 
-//Cliente envia datos al Server
-app.post();
+app.get("/about", (req, res) => {
+  res.send("About page");
+});
+
+// middleware: isAuthenticated
+app.use((req, res, next) => {
+  if (req.query.login === "belen@gmail.com") {
+    next();
+  } else {
+    res.send("Not authorized");
+  }
+});
+
+app.get("/dashboard", (req, res) => {
+  res.send("Dashboard Page");
+});
 
 app.listen(3000, () => {
   console.log("Server running on http://localhost:3000");
